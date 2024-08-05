d7b7ed71e52b07d7a81adc2b43edcac72cf6a1af
Yuragranovsky <yuragranovsky@gmail.com>
2024-08-05 20:32:22 +0300

    synchronized method

--- src/telran/multithreading/CounterUpdater.java
+++ src/telran/multithreading/CounterUpdater.java
@@ -4,28 +4,26 @@ public class CounterUpdater extends Thread {
 	private static long counter;
 	private static final Object mutex = new Object();
 	private int nRuns;
-	
+
 	public CounterUpdater(int nRuns) {
 		this.nRuns = nRuns;
 	}
 
-	@Override 
+	@Override
 	public void run() {
 		for (int i = 0; i < nRuns; i++) {
 			counterIncrement();
 		}
 	}
 
-	private void counterIncrement() {
-		synchronized (mutex) {
-			counter++;
-		}
-		
+	synchronized static private void counterIncrement() {
+		counter++;
+
 	}
 
 	public static long getCounter() {
-		
+
 		return counter;
 	}
-	
+
 }
